ca08187046ec18b2678b4f5ea4a3138822747817
Hanzo15484 <pikach968@gmail.com>
2025-10-29 02:01:18 +0530

    Update force_sub.py

--- force_sub.py
+++ force_sub.py
@@ -213,7 +213,7 @@ async def send_force_sub_message(update: Update, context: ContextTypes.DEFAULT_T
     )
     
     buttons = []
-   for channel in channels:
+    for channel in channels:
         channel_url = (
         channel.get("invite_link")
         or (f"https://t.me/{channel['username']}" if channel.get("username") else f"https://t.me/c/{str(channel['id'])[4:]}")
