8ea96f5d80a7c99df46fa57730c9630d52e2979a
Hanzo15484 <pikach968@gmail.com>
2025-11-07 01:04:00 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -386,10 +386,10 @@ async def settings_button_handler(update: Update, context: ContextTypes.DEFAULT_
             await show_updated_protect_menu(query, False)
 
     elif data == "protect_forward":
-    await query.answer(
-        "ᴘʟᴇᴀsᴇ ᴄʜᴏᴏsᴇ ᴛʜᴇ ᴏᴘᴛɪᴏɴ ʙᴇʟᴏᴡ:",
-        show_alert=True
-    )
+       await query.answer(
+         "ᴘʟᴇᴀsᴇ ᴄʜᴏᴏsᴇ ᴛʜᴇ ᴏᴘᴛɪᴏɴ ʙᴇʟᴏᴡ:",
+         show_alert=True
+      )
     # BACK BUTTON
     elif data == "settings_back":
         settings = load_settings()
