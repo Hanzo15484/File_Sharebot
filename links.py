2adbcb5ff9504b09e22e7c0e3a110e7dc12e375c
Hanzo15484 <pikach968@gmail.com>
2025-11-07 14:14:02 +0530

    Update links.py

--- links.py
+++ links.py
@@ -67,7 +67,7 @@ async def genlink_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # Send initial message
     sent = await update.message.reply_text(
          f"> ᴘʟᴇᴀsᴇ sᴇɴᴅ ᴏʀ ғᴏʀᴡᴀʀᴅ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ ᴀ ʟɪɴᴋ\\.\n"
-         f"ᴛɪᴍᴇᴏᴜᴛ\\: {seconds}s ʀᴇᴍᴀɪɴɪɴɢ",
+         f"ᴛɪᴍᴇᴏᴜᴛ\\: 60s ʀᴇᴍᴀɪɴɪɴɢ",
         parse_mode="Markdown"
      )
 
